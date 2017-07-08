75066b511081823a1e6bb4ebca2c8a1b7042cdb1
Verta-master <vertyagina@gmail.com>
2017-07-08 17:54:51 +0600

    jQuery scroll added

--- js/script.js
+++ js/script.js
@@ -63,3 +63,39 @@ window.addEventListener("keydown", function(event) {
     }
   }
 });
+
+//scroll to menu anchors
+
+$(document).ready(function(){
+  $(".list").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1000);
+  });
+
+//scroll to arrow anchors
+
+  $(".arrow").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 500);
+  });
+  
+//scroll to top
+  
+  $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $(".arrow--back").fadeIn();
+        } else {
+            $(".arrow--back").fadeOut();
+        }
+    });
+  $(".arrow--back").click(function () {
+      $("body,html").animate({
+          scrollTop: 0
+      }, 400);
+      return false;
+  });
+});
