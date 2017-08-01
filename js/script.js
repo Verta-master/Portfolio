7a6f54857dab2814bebfebcddbfd87f5471298bc
Verta-master <vertyagina@gmail.com>
2017-08-01 19:24:00 +0600

    Carousel added

--- js/script.js
+++ js/script.js
@@ -86,6 +86,50 @@ window.addEventListener("keydown", function(event) {
   }
 });
 
+var jsb = document.querySelector(".works__jsb");
+var popup5 = document.querySelector(".works__modal--jsb");
+var close5 = popup5.querySelector(".works__header--jsb");
+
+jsb.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup5.classList.toggle("works__show-jsb");
+});
+
+close5.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup5.classList.remove("works__show-jsb");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup5.classList.contains("works__show-jsb")) {
+      popup5.classList.remove("works__show-jsb");
+    }
+  }
+});
+
+var cre = document.querySelector(".works__creative");
+var popup6 = document.querySelector(".works__modal--creative");
+var close6 = popup6.querySelector(".works__header--creative");
+
+cre.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup6.classList.toggle("works__show-creative");
+});
+
+close6.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup6.classList.remove("works__show-creative");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup6.classList.contains("works__show-creative")) {
+      popup6.classList.remove("works__show-creative");
+    }
+  }
+});
+
 //scroll to menu anchors
 
 $(document).ready(function(){
